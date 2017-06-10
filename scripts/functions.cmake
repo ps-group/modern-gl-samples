67012107a2e874ad4533d2e33cfd5e7f53045e35
Sergey Shambir <sshambir@yandex.ru>
2017-06-10 10:58:08 +0300

    Добился сборки библиотек через CMake

--- scripts/functions.cmake
+++ scripts/functions.cmake
@@ -1,4 +1,14 @@
-cmake_minimum_required(VERSION 3.8 FATAL)
+cmake_minimum_required(VERSION 3.8 FATAL_ERROR)
+
+# Макрос включает статическую компоновку C++ Runtime при сборке с Visual Studio.
+macro(use_static_msvc_runtime)
+    if(MSVC)
+        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
+        set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_MINSIZEREL} /MT")
+        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
+        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /MT")
+    endif(MSVC)
+endmacro()
 
 # В текущей версии CMake не может включить режим C++17 в некоторых компиляторах.
 # Функция использует обходной манёвр.
@@ -11,18 +21,31 @@ function(custom_enable_cxx17 TARGET)
 	endif()
     # Добавляем компоновку с экспериментальной libc++
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+		set_target_properties(${TARGET} PROPERTIES COMPILE_FLAGS "-std=c++1z")
         target_link_libraries(${TARGET} c++experimental)
     endif()
 endfunction(custom_enable_cxx17)
 
+# Функция добавляет вспомогательную библиотеку из курса OpenGL.
+function(add_gl_library TARGET)
+    # -- оставлено для отладки --
+    # message("CMAKE_CURRENT_SOURCE_DIR = ${CMAKE_CURRENT_SOURCE_DIR}")
+    # message("CMAKE_SOURCE_DIR = ${CMAKE_SOURCE_DIR}")
+
+    # Собираем файлы с текущего каталога
+    file(GLOB TARGET_SRC "${CMAKE_CURRENT_SOURCE_DIR}/*.cpp" "${CMAKE_CURRENT_SOURCE_DIR}/*.h")
+    add_library(${TARGET} ${TARGET_SRC})
+    custom_enable_cxx17(${TARGET})
+endfunction()
+
 # Функция добавляет пример из курса OpenGL
 function(add_gl_example TARGET)
     # Собираем файлы с текущего каталога
-    file(GLOB TARGET_SRC "*.cpp" "*.h")
+    file(GLOB TARGET_SRC "CMAKE_CURRENT_SOURCE_DIR/*.cpp" "CMAKE_CURRENT_SOURCE_DIR/*.h")
     # Составляем список библиотек
     set(TARGET_LIBS SDL2 SDL2_image SDL2_ttf SDL2_mixer GL glbinding assimp)
     # Составляем список путей поиска заголовочных файлов
-    set(TARGET_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/../libs)
+    set(TARGET_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/libs)
 
     add_executable(${TARGET} ${TARGET_SRC})
     custom_enable_cxx17(${TARGET})
